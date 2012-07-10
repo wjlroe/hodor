0cfe7e4ad25da7d370cf9da1b6b3c8cff955dc3a
William Roe <willroe@gmail.com>
2012-07-10 18:04:06 +0100

    Make script work

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-which vagrant || echo "You need to install the vagrant gem"; exit 1
+set -e
 
-cd graylo2_server
+which vagrant || (echo "You need to install the vagrant gem"; exit 1)
+
+cd graylog2_server
 mkdir -p cookbooks
-if [ -d graylog2 ];
-    cd graylog2 && git pull
+cd cookbooks
+if [ -d graylog2 ]; then
+    cd graylog2 && git pull;
 else
-    git co git://github.com/nilya/graylog2.cookbook.git graylog2
+    git clone "https://github.com/nilya/graylog2.cookbook.git" graylog2
 fi
-
-cd graylog2_server && vagrant init
